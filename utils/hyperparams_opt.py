2fb09291d72325a3c8b34180cfe36c8efc979c9e
Antonin Raffin <antonin.raffin@dlr.de>
2019-11-04 17:10:57 +0100

    Hyperparams optimization for A2C

--- utils/hyperparams_opt.py
+++ utils/hyperparams_opt.py
@@ -11,6 +11,8 @@ from torchy_baselines.common.vec_env import VecNormalize, VecEnv
 # from torchy_baselines.her import HERGoalEnvWrapper
 # from torchy_baselines.common.base_class import _UnvecWrapper
 
+from utils import linear_schedule
+
 
 def hyperparam_optimization(algo, model_fn, env_fn, n_trials=10, n_timesteps=5000, hyperparams=None,
                             n_jobs=1, sampler_method='random', pruner_method='halving',
@@ -226,21 +228,38 @@ def sample_a2c_params(trial):
     :return: (dict)
     """
     gamma = trial.suggest_categorical('gamma', [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
+    normalize_advantage = trial.suggest_categorical('normalize_advantage', [False, True])
+    max_grad_norm = trial.suggest_categorical('max_grad_norm', [0.5, 1, 5, 10])
+    use_rms_prop = trial.suggest_categorical('use_rms_prop', [False, True])
+    gae_lambda = trial.suggest_categorical('gae_lambda', [0.8, 0.9, 0.92, 0.95, 0.98, 0.99, 1.0])
     n_steps = trial.suggest_categorical('n_steps', [8, 16, 32, 64, 128, 256, 512, 1024, 2048])
     lr_schedule = trial.suggest_categorical('lr_schedule', ['linear', 'constant'])
     learning_rate = trial.suggest_loguniform('lr', 1e-5, 1)
     ent_coef = trial.suggest_loguniform('ent_coef', 0.00000001, 0.1)
     vf_coef = trial.suggest_uniform('vf_coef', 0, 1)
-    # normalize = trial.suggest_categorical('normalize', [True, False])
-    # TODO: take into account the normalization (also for the test env)
+    log_std_init = trial.suggest_uniform('log_std_init', 0, 1)
+    ortho_init = trial.suggest_categorical('ortho_init', [False, True])
+    net_arch = trial.suggest_categorical('net_arch', ["big", "small"])
+
+    if lr_schedule == 'linear':
+        learning_rate = linear_schedule(learning_rate)
+
+    if net_arch == 'big':
+        net_arch = [dict(pi=[256, 256], vf=[256, 256])]
+    else:
+        net_arch = [dict(pi=[64, 64], vf=[64, 64])]
 
     return {
         'n_steps': n_steps,
         'gamma': gamma,
+        'gae_lambda': gae_lambda,
         'learning_rate': learning_rate,
-        'lr_schedule': lr_schedule,
         'ent_coef': ent_coef,
-        'vf_coef': vf_coef
+        'normalize_advantage': normalize_advantage,
+        'max_grad_norm': max_grad_norm,
+        'use_rms_prop': use_rms_prop,
+        'vf_coef': vf_coef,
+        'policy_kwargs': dict(log_std_init=log_std_init, net_arch=net_arch)
     }
 
 
