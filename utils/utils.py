7576e8ae2c8a4da985d6a72575278d0eb209ce49
Antonin Raffin <antonin.raffin@dlr.de>
2020-01-10 14:26:06 +0100

    Enable multiple wrappers

--- utils/utils.py
+++ utils/utils.py
@@ -38,10 +38,17 @@ def flatten_dict_observations(env):
 
 def get_wrapper_class(hyperparams):
     """
-    Get a Gym environment wrapper class specified as a hyper parameter
+    Get one or more Gym environment wrapper class specified as a hyper parameter
     "env_wrapper".
     e.g.
     env_wrapper: gym_minigrid.wrappers.FlatObsWrapper
+
+    for multiple, specify a list:
+
+    env_wrapper:
+        - utils.wrappers.PlotActionWrapper
+        - utils.wrappers.TimeFeatureWrapper
+
 
     :param hyperparams: (dict)
     :return: a subclass of gym.Wrapper (class object) you can use to
@@ -56,8 +63,27 @@ def get_wrapper_class(hyperparams):
 
     if 'env_wrapper' in hyperparams.keys():
         wrapper_name = hyperparams.get('env_wrapper')
-        wrapper_module = importlib.import_module(get_module_name(wrapper_name))
-        return getattr(wrapper_module, get_class_name(wrapper_name))
+        if not isinstance(wrapper_name, list):
+            wrapper_names = [wrapper_name]
+        else:
+            wrapper_names = wrapper_name
+
+        wrapper_classes = []
+        # Handle multiple wrappers
+        for wrapper_name in wrapper_names:
+            wrapper_module = importlib.import_module(get_module_name(wrapper_name))
+            wrapper_class = getattr(wrapper_module, get_class_name(wrapper_name))
+            wrapper_classes.append(wrapper_class)
+
+        def wrap_env(env):
+            """
+            :param env: (gym.Env)
+            :return: (gym.Env)
+            """
+            for wrapper_class in wrapper_classes:
+                env = wrapper_class(env)
+            return env
+        return wrap_env
     else:
         return None
 
