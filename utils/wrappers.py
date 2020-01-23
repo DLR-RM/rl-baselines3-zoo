c99eac878f68589cf1a7bd3916558b8d5b838d0f
Antonin Raffin <antonin.raffin@dlr.de>
2020-01-23 11:32:58 +0100

    Add history wrapper

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -102,12 +102,11 @@ class DelayedRewardWrapper(gym.Wrapper):
     The reward is accumulated during those steps.
 
     :param env: (gym.Env)
-    :param delay: (float) Number of steps the reward should be delayed.
+    :param delay: (int) Number of steps the reward should be delayed.
     """
 
     def __init__(self, env, delay=10):
         super(DelayedRewardWrapper, self).__init__(env)
-        print("DelayedRewardWrapper", "delay=", delay)
         self.delay = delay
         self.current_step = 0
         self.accumulated_reward = 0.0
@@ -129,6 +128,66 @@ class DelayedRewardWrapper(gym.Wrapper):
         else:
             reward = 0.0
         return obs, reward, done, info
+
+
+class HistoryWrapper(gym.Wrapper):
+    """
+    Delay the reward by `delay` steps, it makes the task harder but more realistic.
+    The reward is accumulated during those steps.
+
+    :param env: (gym.Env)
+    :param horizon: (int) Number of steps to keep in the history.
+    """
+
+    def __init__(self, env, horizon=5):
+        assert isinstance(env.observation_space, gym.spaces.Box)
+
+        wrapped_obs_space = env.observation_space
+        wrapped_action_space = env.action_space
+
+        # TODO: double check, it seems wrong when we have different low and highs
+        low_obs = np.repeat(wrapped_obs_space.low, horizon, axis=-1)
+        high_obs = np.repeat(wrapped_obs_space.high, horizon, axis=-1)
+
+        low_action = np.repeat(wrapped_action_space.low, horizon, axis=-1)
+        high_action = np.repeat(wrapped_action_space.high, horizon, axis=-1)
+
+        low = np.concatenate((low_obs, low_action))
+        high = np.concatenate((high_obs, high_action))
+
+        # Overwrite the observation space
+        env.observation_space = gym.spaces.Box(low=low, high=high, dtype=wrapped_obs_space.dtype)
+
+        super(HistoryWrapper, self).__init__(env)
+
+        self.horizon = horizon
+        self.low_action, self.high_action = low_action, high_action
+        self.low_obs, self.high_obs = low_obs, high_obs
+        self.low, self.high = low, high
+        self.obs_history = np.zeros(low_obs.shape, low_obs.dtype)
+        self.action_history = np.zeros(low_action.shape, low_action.dtype)
+
+    def _create_obs_from_history(self):
+        return np.concatenate((self.obs_history, self.action_history))
+
+    def reset(self):
+        # Flush the history
+        self.obs_history[...] = 0
+        self.action_history[...] = 0
+        obs = self.env.reset()
+        self.obs_history[..., -obs.shape[-1]:] = obs
+        return self._create_obs_from_history()
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        last_ax_size = obs.shape[-1]
+
+        self.obs_history = np.roll(self.obs_history, shift=-last_ax_size, axis=-1)
+        self.obs_history[..., -obs.shape[-1]:] = obs
+
+        self.action_history = np.roll(self.action_history, shift=-action.shape[-1], axis=-1)
+        self.action_history[..., -action.shape[-1]:] = action
+        return self._create_obs_from_history(), reward, done, info
 
 
 class PlotActionWrapper(gym.Wrapper):
