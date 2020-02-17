c81985bdb63029f59650e4812e4d24867b4ba4f5
Antonin Raffin <antonin.raffin@dlr.de>
2020-02-17 16:10:48 +0100

    Improve plot style

--- utils/all_plots.py
+++ utils/all_plots.py
@@ -7,35 +7,44 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 import pytablewriter
 import numpy as np
+import seaborn
 import matplotlib.pyplot as plt
 
 from torchy_baselines.common.results_plotter import *
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-a', '--algos', nargs='+', type=str)
-parser.add_argument('-e', '--env', nargs='+', type=str)
-parser.add_argument('-f', '--exp_folder', nargs='+', type=str)
-parser.add_argument('-l', '--labels', nargs='+', default=['sde', 'gaussian'], type=str)
+parser = argparse.ArgumentParser('Gather results, plot them and create table')
+parser.add_argument('-a', '--algos', help='Algorithms to include', nargs='+', type=str)
+parser.add_argument('-e', '--env', help='Environments to include', nargs='+', type=str)
+parser.add_argument('-f', '--exp_folder', help='Folders to include', nargs='+', type=str)
+parser.add_argument('-l', '--labels', help='Label for each folder', nargs='+', default=['sde', 'gaussian'], type=str)
+parser.add_argument('-median', '--median', action='store_true', default=False,
+                    help='Display median instead of mean in the table')
 args = parser.parse_args()
 
+# Activate seaborn
+seaborn.set()
 results = {}
+
+args.algos = [algo.upper() for algo in args.algos]
 
 for env in args.env:
     plt.figure(f'Results {env}')
-    plt.title(f'{env}')
+    plt.title(f'{env}BulletEnv-v0', fontsize=14)
+    plt.xlabel('Timesteps (in Millions)', fontsize=14)
+    plt.ylabel('Score', fontsize=14)
     results[env] = {}
-    for folder_idx, exp_folder in enumerate(args.exp_folder):
-        for algo in args.algos:
+    for algo in args.algos:
+        for folder_idx, exp_folder in enumerate(args.exp_folder):
 
             results[env][f'{args.labels[folder_idx]}-{algo}'] = 0.0
-            log_path = os.path.join(exp_folder, algo)
+            log_path = os.path.join(exp_folder, algo.lower())
 
             dirs = [os.path.join(log_path, d) for d in os.listdir(log_path) if env in d]
 
             max_len = 0
-            merged_mean = []
-            merged_std = []
+            merged_mean, merged_std = [], []
+            last_eval = []
             timesteps = None
             for idx, dir_ in enumerate(dirs):
                 try:
@@ -45,31 +54,47 @@ for env in args.env:
                     continue
 
                 mean_ = np.squeeze(log['results'].mean(axis=1))
+                #
+                # TODO: Compute standard error for all the runs
+                # std_ = np.squeeze(log['results'].std(axis=1)) / np.sqrt(log['results'].shape[1])
                 std_ = np.squeeze(log['results'].std(axis=1))
 
-                max_len = max(max_len, len(mean_))
                 merged_mean.append(mean_)
                 merged_std.append(std_)
+                last_eval.append(log['results'][-1])
+
+                max_len = max(max_len, len(mean_))
                 if len(log['timesteps']) == max_len:
                     timesteps = log['timesteps']
 
             # Remove incomplete runs
-            mean_tmp, std_tmp = [], []
+            mean_tmp, std_tmp, last_eval_tmp = [], [], []
             for idx in range(len(merged_mean)):
                 if len(merged_mean[idx]) == max_len:
                     mean_tmp.append(merged_mean[idx])
                     std_tmp.append(merged_std[idx])
+                    last_eval_tmp.append(last_eval[idx])
             merged_mean = mean_tmp
             merged_std = std_tmp
+            last_eval = last_eval_tmp
 
             if len(merged_mean) > 0:
                 mean_ = np.mean(merged_mean, axis=0)
-                std_ = np.mean(merged_std, axis=0)
-                # Take last evaluationqq
-                results[env][f'{algo}-{args.labels[folder_idx]}'] = mean_[-1]
+                # TODO: fix standard error computation
+                std_ = np.mean(merged_std, axis=0) / np.sqrt(len(merged_mean))
+                # Take last evaluation
+                # Compute standard error: std / sqrt(n_runs)
+                std_error = np.std(last_eval) / np.sqrt(len(last_eval))
 
-                plt.plot(timesteps, mean_, label=f'{algo}-{args.labels[folder_idx]}')
-                plt.fill_between(timesteps, mean_ + std_, mean_ - std_, alpha=0.5)
+                if args.median:
+                    results[env][f'{algo}-{args.labels[folder_idx]}'] = f'{np.median(last_eval):.0f}'
+                else:
+                    results[env][f'{algo}-{args.labels[folder_idx]}'] = f'{np.mean(last_eval):.0f} +/- {std_error:.0f}'
+
+
+                # x axis in Millions of timesteps
+                plt.plot(timesteps / 1e6, mean_, label=f'{algo}-{args.labels[folder_idx]}')
+                plt.fill_between(timesteps / 1e6, mean_ + std_, mean_ - std_, alpha=0.5)
 
     plt.legend()
 
@@ -98,7 +123,7 @@ for i, env in enumerate(args.env, start=1):
     for algo in args.algos:
         for label in args.labels:
             key = f'{algo}-{label}'
-            value_matrix[i].append(f'{results[env].get(key, 0.0):.2f}')
+            value_matrix[i].append(f'{results[env].get(key, "0.0 +/- 0.0")}')
 
 writer.value_matrix = value_matrix
 writer.write_table()
