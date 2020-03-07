a34529175cf6ba247b7225da6c38b39ba924f298
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-03-07 21:33:47 +0100

    Fix for overwritting env wrapper

--- utils/utils.py
+++ utils/utils.py
@@ -63,6 +63,10 @@ def get_wrapper_class(hyperparams):
 
     if 'env_wrapper' in hyperparams.keys():
         wrapper_name = hyperparams.get('env_wrapper')
+
+        if wrapper_name is None:
+            return None
+
         if not isinstance(wrapper_name, list):
             wrapper_names = [wrapper_name]
         else:
@@ -127,6 +131,10 @@ def get_callback_class(hyperparams):
 
     if 'callback' in hyperparams.keys():
         callback_name = hyperparams.get('callback')
+
+        if callback_name is None:
+            return None
+
         if not isinstance(callback_name, list):
             callback_names = [callback_name]
         else:
