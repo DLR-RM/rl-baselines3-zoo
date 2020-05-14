d9d52dfc18963b0e536d296a84abe59130421fd9
Salman Shaikh <tony903212@gmail.com>
2020-05-15 00:38:36 +0530

    Refactored benchmark.py (#7)
    * Refactored benchmark.py
    now using f-string everywhere
    
    * More Cleaner parser.add_arguments
    
    * Added TODO.
    
    Co-authored-by: Antonin RAFFIN <antonin.raffin@ensta.org>

--- utils/benchmark.py
+++ utils/benchmark.py
@@ -18,17 +18,42 @@ from utils import get_trained_models
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--log-dir', help='Root log folder', default='rl-trained-agents/', type=str)
-parser.add_argument('--benchmark-dir', help='Benchmark log folder', default='logs/benchmark/', type=str)
-parser.add_argument('-n', '--n-timesteps', help='number of timesteps', default=150000,
-                    type=int)
-parser.add_argument('--n-envs', help='number of environments', default=1,
-                    type=int)
-parser.add_argument('--verbose', help='Verbose mode (0: no output, 1: INFO)', default=1,
-                    type=int)
-parser.add_argument('--seed', help='Random generator seed', type=int, default=0)
-parser.add_argument('--test-mode', action='store_true', default=False,
-                    help='Do only one experiments (useful for testing)')
+parser.add_argument(
+    '--log-dir',
+    help='Root log folder',
+    default='rl-trained-agents/',
+    type=str)
+parser.add_argument(
+    '--benchmark-dir',
+    help='Benchmark log folder',
+    default='logs/benchmark/',
+    type=str)
+parser.add_argument(
+    '-n',
+    '--n-timesteps',
+    help='number of timesteps',
+    default=150000,
+    type=int)
+parser.add_argument(
+    '--n-envs',
+    help='number of environments',
+    default=1,
+    type=int)
+parser.add_argument(
+    '--verbose',
+    help='Verbose mode (0: no output, 1: INFO)',
+    default=1,
+    type=int)
+parser.add_argument(
+    '--seed',
+    help='Random generator seed',
+    type=int,
+    default=0)
+parser.add_argument(
+    '--test-mode',
+    action='store_true',
+    default=False,
+    help='Do only one experiments (useful for testing)')
 args = parser.parse_args()
 
 trained_models = get_trained_models(args.log_dir)
@@ -53,10 +78,10 @@ for idx, trained_model in enumerate(trained_models.keys()):
     # Comment out to becnhmark HER robotics env
     # this requires a mujoco licence
     if 'Fetch' in env_id:
-        print("Skipping mujoco env: {}".format(env_id))
+        print(f"Skipping mujoco env: {env_id}")
         continue
 
-    reward_log = '{}/{}/'.format(args.benchmark_dir, trained_model)
+    reward_log = f"{args.benchmark_dir}/{trained_model}/"
     arguments = [
         '-n', str(n_timesteps),
         '--n-envs', str(n_envs),
@@ -69,14 +94,15 @@ for idx, trained_model in enumerate(trained_models.keys()):
         '--reward-log', reward_log
     ]
     if args.verbose >= 1:
-        print('{}/{}'.format(idx + 1, n_experiments))
-        print("Evaluating {} on {}...".format(algo, env_id))
+        print(f"{idx + 1}/{n_experiments}")
+        print(f"Evaluating {algo} on {env_id}...")
 
     skip_eval = False
     if os.path.isdir(reward_log):
         try:
             x, y = ts2xy(load_results(reward_log), 'timesteps')
             skip_eval = len(x) > 0
+        # TODO: fix this bare except
         except:
             pass
 
@@ -96,9 +122,9 @@ for idx, trained_model in enumerate(trained_models.keys()):
         results['n_timesteps'].append(x[-1])
         results['n_episodes'].append(len(y))
         if args.verbose >= 1:
-            print(x[-1], 'timesteps')
+            print(x[-1], "timesteps")
             print(len(y), "Episodes")
-            print("Mean reward: {:.2f} +- {:.2f}".format(mean_reward, std_reward))
+            print(f"Mean reward: {mean_reward:.2f} +- {std_reward:.2f}")
             print()
     else:
         print("Not enough timesteps")
@@ -114,10 +140,10 @@ results_df = results_df.sort_values(by=['algo', 'env_id'])
 writer = pytablewriter.MarkdownTableWriter()
 writer.from_dataframe(results_df)
 # change the output stream to a file
-with open("{}/benchmark.md".format(args.benchmark_dir), "w") as f:
+with open(f"{args.benchmark_dir}/benchmark.md", "w") as f:
     writer.stream = f
     writer.write_table()
-print("Results written to:", "{}/benchmark.md".format(args.benchmark_dir))
+print(f"Results written to:", "{args.benchmark_dir}/benchmark.md")
 
 # Update root benchmark file
 with open("benchmark.md", "w") as f:
@@ -125,5 +151,5 @@ with open("benchmark.md", "w") as f:
     writer.write_table()
 
 # Alternatively, to dump as csv file:
-# results_df.to_csv('{}/benchmark.csv'.format(args.benchmark_dir), sep=",", index=False)
-# print("Saved results to {}/benchmark.csv".format(args.benchmark_dir))
+# results_df.to_csv(f"{args.benchmark_dir}/benchmark.csv",sep=",", index=False)
+# print("Saved results to {args.benchmark_dir}/benchmark.csv")
