1327105c1ca659f1ef612c73c6755169211df8b3
Megan Klaiber <megan.klaiber@outlook.com>
2020-08-28 17:26:05 +0200

    Added History Wrapper for dict observation. Updated success plotting.

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -1,6 +1,5 @@
 import gym
 import numpy as np
-from gym.wrappers import TimeLimit
 from matplotlib import pyplot as plt
 
 
@@ -190,6 +189,66 @@ class HistoryWrapper(gym.Wrapper):
         self.action_history = np.roll(self.action_history, shift=-action.shape[-1], axis=-1)
         self.action_history[..., -action.shape[-1] :] = action
         return self._create_obs_from_history(), reward, done, info
+
+
+class HistoryWrapperObsDict(gym.Wrapper):
+    def __init__(self, env, horizon=5):
+        assert isinstance(env.observation_space.spaces["observation"], gym.spaces.Box)
+
+        wrapped_obs_space = env.observation_space.spaces["observation"]
+        wrapped_action_space = env.action_space
+
+        # TODO: double check, it seems wrong when we have different low and highs
+        low_obs = np.repeat(wrapped_obs_space.low, horizon, axis=-1)
+        high_obs = np.repeat(wrapped_obs_space.high, horizon, axis=-1)
+
+        low_action = np.repeat(wrapped_action_space.low, horizon, axis=-1)
+        high_action = np.repeat(wrapped_action_space.high, horizon, axis=-1)
+
+        low = np.concatenate((low_obs, low_action))
+        high = np.concatenate((high_obs, high_action))
+
+        # Overwrite the observation space
+        env.observation_space.spaces["observation"] = gym.spaces.Box(low=low, high=high, dtype=wrapped_obs_space.dtype)
+
+        super(HistoryWrapperObsDict, self).__init__(env)
+
+        self.horizon = horizon
+        self.low_action, self.high_action = low_action, high_action
+        self.low_obs, self.high_obs = low_obs, high_obs
+        self.low, self.high = low, high
+        self.obs_history = np.zeros(low_obs.shape, low_obs.dtype)
+        self.action_history = np.zeros(low_action.shape, low_action.dtype)
+
+    def _create_obs_from_history(self):
+        return np.concatenate((self.obs_history, self.action_history))
+
+    def reset(self):
+        # Flush the history
+        self.obs_history[...] = 0
+        self.action_history[...] = 0
+        obs_dict = self.env.reset()
+        obs = obs_dict["observation"]
+        self.obs_history[..., -obs.shape[-1] :] = obs
+
+        obs_dict["observation"] = self._create_obs_from_history()
+
+        return obs_dict
+
+    def step(self, action):
+        obs_dict, reward, done, info = self.env.step(action)
+        obs = obs_dict["observation"]
+        last_ax_size = obs.shape[-1]
+
+        self.obs_history = np.roll(self.obs_history, shift=-last_ax_size, axis=-1)
+        self.obs_history[..., -obs.shape[-1] :] = obs
+
+        self.action_history = np.roll(self.action_history, shift=-action.shape[-1], axis=-1)
+        self.action_history[..., -action.shape[-1] :] = action
+
+        obs_dict["observation"] = self._create_obs_from_history()
+
+        return obs_dict, reward, done, info
 
 
 class PlotActionWrapper(gym.Wrapper):
