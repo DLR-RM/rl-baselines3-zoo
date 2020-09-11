b0996b3e8257d2539c5a9cb901cd6485493cc7e8
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-09-11 18:40:32 +0200

    Add action smoothing and low pass filter

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -97,6 +97,101 @@ class ActionNoiseWrapper(gym.Wrapper):
         return self.env.step(noisy_action)
 
 
+# from https://docs.obspy.org
+from scipy.signal import iirfilter, sosfilt, zpk2sos
+
+
+def lowpass(data, freq, df, corners=4, zerophase=False):
+    """
+    Butterworth-Lowpass Filter.
+
+    Filter data removing data over certain frequency ``freq`` using ``corners``
+    corners.
+    The filter uses :func:`scipy.signal.iirfilter` (for design)
+    and :func:`scipy.signal.sosfilt` (for applying the filter).
+
+    :type data: numpy.ndarray
+    :param data: Data to filter.
+    :param freq: Filter corner frequency.
+    :param df: Sampling rate in Hz.
+    :param corners: Filter corners / order.
+    :param zerophase: If True, apply filter once forwards and once backwards.
+        This results in twice the number of corners but zero phase shift in
+        the resulting filtered trace.
+    :return: Filtered data.
+    """
+    fe = 0.5 * df
+    f = freq / fe
+    # raise for some bad scenarios
+    if f > 1:
+        f = 1.0
+        msg = "Selected corner frequency is above Nyquist. " + "Setting Nyquist as high corner."
+        print(msg)
+    z, p, k = iirfilter(corners, f, btype="lowpass", ftype="butter", output="zpk")
+    sos = zpk2sos(z, p, k)
+    if zerophase:
+        firstpass = sosfilt(sos, data)
+        return sosfilt(sos, firstpass[::-1])[::-1]
+    else:
+        return sosfilt(sos, data)
+
+
+class LowPassFilterWrapper(gym.Wrapper):
+    """
+    Butterworth-Lowpass
+
+    :param env: (gym.Env)
+    :param freq: Filter corner frequency.
+    :param df: Sampling rate in Hz.
+    """
+
+    def __init__(self, env, freq=5.0, df=25.0):
+        super(LowPassFilterWrapper, self).__init__(env)
+        self.freq = freq
+        self.df = df
+        self.signal = []
+
+    def reset(self):
+        self.signal = []
+        return self.env.reset()
+
+    def step(self, action):
+        self.signal.append(action)
+        filtered = np.zeros_like(action)
+        for i in range(self.action_space.shape[0]):
+            smoothed_action = lowpass(np.array(self.signal)[:, i], freq=self.freq, df=self.df)
+            filtered[i] = smoothed_action[-1]
+        return self.env.step(filtered)
+
+
+class ActionSmoothingWrapper(gym.Wrapper):
+    """
+    Smooth the action using exponential moving average.
+
+    :param env: (gym.Env)
+    :param smoothing_coef: (float) Smoothing coefficient (0 no smoothing, 1 very smooth)
+    """
+
+    def __init__(self, env, smoothing_coef: float = 0.0):
+        super(ActionSmoothingWrapper, self).__init__(env)
+        self.smoothing_coef = smoothing_coef
+        self.smoothed_action = None
+        # from https://github.com/rail-berkeley/softlearning/issues/3
+        # for smoothing latent space
+        # self.alpha = self.smoothing_coef
+        # self.beta = np.sqrt(1 - self.alpha ** 2) / (1 - self.alpha)
+
+    def reset(self):
+        self.smoothed_action = None
+        return self.env.reset()
+
+    def step(self, action):
+        if self.smoothed_action is None:
+            self.smoothed_action = np.zeros_like(action)
+        self.smoothed_action = self.smoothing_coef * self.smoothed_action + (1 - self.smoothing_coef) * action
+        return self.env.step(self.smoothed_action)
+
+
 class DelayedRewardWrapper(gym.Wrapper):
     """
     Delay the reward by `delay` steps, it makes the task harder but more realistic.
