1ebfc8bb6bffe49e5b03ae165d87b6eb513f8e8d
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-09-25 11:42:31 +0200

    Update plot script

--- scripts/plot_train.py
+++ scripts/plot_train.py
@@ -5,7 +5,7 @@ import argparse
 import os
 
 from matplotlib import pyplot as plt
-from stable_baselines3.common.results_plotter import X_TIMESTEPS, plot_results
+from stable_baselines3.common.results_plotter import X_EPISODES, X_TIMESTEPS, X_WALLTIME, plot_results
 
 # For tensorflow imported with tensorboard
 # import warnings
@@ -16,12 +16,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("algo", type=str)
 parser.add_argument("env", type=str)
 parser.add_argument("exp_folder", type=str)
+parser.add_argument("axis", choices=["steps", "episodes", "time"], type=str)
 args = parser.parse_args()
 
 
 algo = args.algo
 env = args.env
 log_path = os.path.join(args.exp_folder, algo)
+x_axis = {"steps": X_TIMESTEPS, "episodes": X_EPISODES, "time": X_WALLTIME}[args.axis]
 
 dirs = [
     os.path.join(log_path, folder)
@@ -30,7 +32,7 @@ dirs = [
 ]
 
 try:
-    plot_results(dirs, 2e6, X_TIMESTEPS, env)
+    plot_results(dirs, 2e6, x_axis, env)
 except Exception as e:
     print(e)
 
