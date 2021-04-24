46a3ab622a59dcabd28c5dddd432ffbeb3c3d721
Justin Terry <justinkterry@gmail.com>
2021-04-24 10:29:43 -0400

    try linear interpolation

--- utils/exp_manager.py
+++ utils/exp_manager.py
@@ -479,7 +479,7 @@ class ExperimentManager(object):
 
         env = pistonball_v4.parallel_env(time_penalty=-1)
         env = ss.color_reduction_v0(env, mode='B')
-        env = ss.resize_v0(env, x_size=84, y_size=84)
+        env = ss.resize_v0(env, x_size=84, y_size=84, linear_interp=True)
         env = ss.frame_stack_v1(env, 3)
         env = ss.pettingzoo_env_to_vec_env_v0(env)
         env = ss.concat_vec_envs_v0(env, n_envs, num_cpus=4, base_class='stable_baselines3')
