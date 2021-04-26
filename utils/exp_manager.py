af2dbf8924a8ddcd70215b6f47f9cccf65126ecc
Justin Terry <justinkterry@gmail.com>
2021-04-26 00:56:32 -0400

    lower n pistons test

--- utils/exp_manager.py
+++ utils/exp_manager.py
@@ -477,12 +477,12 @@ class ExperimentManager(object):
 
     def create_envs(self, n_envs: int, eval_env: bool = False, no_log: bool = False) -> VecEnv:
 
-        env = pistonball_v4.parallel_env(time_penalty=-1)
+        env = pistonball_v4.parallel_env(time_penalty=-1, n_pistons=10)
         env = ss.color_reduction_v0(env, mode='B')
         env = ss.resize_v0(env, x_size=84, y_size=84, linear_interp=True)
         env = ss.frame_stack_v1(env, 3)
         env = ss.pettingzoo_env_to_vec_env_v0(env)
-        env = ss.concat_vec_envs_v0(env, n_envs, base_class='stable_baselines3')
+        env = ss.concat_vec_envs_v0(env, n_envs, num_cpus=4, base_class='stable_baselines3')
 
         env = self._maybe_normalize(env, eval_env)
 
