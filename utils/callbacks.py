aa6d934a42fbd9ff613a2a0b8338f940a5c4af32
Antonin Raffin <antonin.raffin@ensta.org>
2021-05-06 14:40:55 +0200

    Take learning starts into account

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -137,6 +137,7 @@ class ParallelTrainCallback(BaseCallback):
             self._model.replay_buffer = deepcopy(self.model.replay_buffer)
             self.model.set_parameters(deepcopy(self._model.get_parameters()))
             self.model.actor = self.model.policy.actor
-            self.train()
+            if self.num_timesteps >= self._model.learning_starts:
+                self.train()
             # Do not wait for the training loop to finish
             # self.process.join()
