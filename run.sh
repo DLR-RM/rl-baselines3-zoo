00e8f33cc3ce26ef201e0a9fe15ebd66a66daf3c
Justin Terry <justinkterry@gmail.com>
2021-06-02 15:39:12 -0400

    add sleeping

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 4000000 -optimize --n-trials 1000 --sampler tpe --pruner median --study-name pistonball18 --storage mysql://root:dummy@10.128.0.28/pistonball18 &> 0.out &
-
+sleep 3
 CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 4000000 -optimize --n-trials 1000 --sampler tpe --pruner median --study-name pistonball18 --storage mysql://root:dummy@10.128.0.28/pistonball18 &> 1.out &
-
+sleep 3
 CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 4000000 -optimize --n-trials 1000 --sampler tpe --pruner median --study-name pistonball18 --storage mysql://root:dummy@10.128.0.28/pistonball18 &> 2.out &
-
+sleep 3
 CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 4000000 -optimize --n-trials 1000 --sampler tpe --pruner median --study-name pistonball18 --storage mysql://root:dummy@10.128.0.28/pistonball18 &> 3.out &
-
+sleep 3
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 4000000 -optimize --n-trials 1000 --sampler tpe --pruner median --study-name pistonball18 --storage mysql://root:dummy@10.128.0.28/pistonball18 &> 4.out &
-
+sleep 3
 CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 4000000 -optimize --n-trials 1000 --sampler tpe --pruner median --study-name pistonball18 --storage mysql://root:dummy@10.128.0.28/pistonball18 &> 5.out &
-
+sleep 3
 CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 4000000 -optimize --n-trials 1000 --sampler tpe --pruner median --study-name pistonball18 --storage mysql://root:dummy@10.128.0.28/pistonball18 &> 6.out &
-
+sleep 3
 CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 4000000 -optimize --n-trials 1000 --sampler tpe --pruner median --study-name pistonball18 --storage mysql://root:dummy@10.128.0.28/pistonball18 &> 7.out &
+sleep 3
