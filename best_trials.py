0f69bc92213e13b25a7eeb073297127f7bf9e417
Justin Terry <justinkterry@gmail.com>
2021-06-02 15:30:59 -0400

    fix

--- best_trials.py
+++ best_trials.py
@@ -1,15 +1,16 @@
 import optuna
 study = optuna.create_study(study_name='pistonball15', storage='mysql://root:dummy@10.128.0.28/pistonball15', load_if_exists=True, direction="maximize")
-for i in study.trials:
-    print(i.value)
-
-print(study.trials)
 print(study.best_trial)
-
-trial = study.best_trial
-
-optuna.study.StudySummary(study)
-summary = optuna.study.get_all_study_summaries('mysql://root:dummy@10.128.0.28/pistonball15')
-
 for i in study.trials:
     print(i.value)
+
+# print(study.trials)
+
+
+# trial = study.best_trial
+
+# optuna.study.StudySummary(study)
+# summary = optuna.study.get_all_study_summaries('mysql://root:dummy@10.128.0.28/pistonball15')
+
+# for i in study.trials:
+#     print(i.value)
