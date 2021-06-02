4dd18119110cb17cb3b3a97ed4e855eca049215d
Justin Terry <justinkterry@gmail.com>
2021-06-02 15:36:11 -0400

    switch to 16

--- best_trials.py
+++ best_trials.py
@@ -1,5 +1,5 @@
 import optuna
-study = optuna.create_study(study_name='pistonball15', storage='mysql://root:dummy@10.128.0.28/pistonball15', load_if_exists=True, direction="maximize")
+study = optuna.create_study(study_name='pistonball18', storage='mysql://root:dummy@10.128.0.28/pistonball18', load_if_exists=True, direction="maximize")
 print(study.best_trial)
 for i in study.trials:
     print(i.value)
@@ -10,7 +10,7 @@ for i in study.trials:
 # trial = study.best_trial
 
 # optuna.study.StudySummary(study)
-# summary = optuna.study.get_all_study_summaries('mysql://root:dummy@10.128.0.28/pistonball15')
+# summary = optuna.study.get_all_study_summaries('mysql://root:dummy@10.128.0.28/pistonball18')
 
 # for i in study.trials:
 #     print(i.value)
