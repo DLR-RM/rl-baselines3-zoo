00d7cd9bb2c722f7b05e551f196a51e62cae584f
Antonin Raffin <antonin.raffin@ensta.org>
2021-06-04 11:47:35 +0200

    Add Phase Feature

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -1,9 +1,11 @@
 from copy import deepcopy
+from typing import Union
 
 import gym
 import numpy as np
 from sb3_contrib.common.wrappers import TimeFeatureWrapper  # noqa: F401 (backward compatibility)
 from scipy.signal import iirfilter, sosfilt, zpk2sos
+from stable_baselines3.common.type_aliases import GymObs, GymStepReturn
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv, VecEnvObs, VecEnvStepReturn, VecEnvWrapper
 from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv, _flatten_obs
 
@@ -371,3 +373,43 @@ class HistoryWrapperObsDict(gym.Wrapper):
         obs_dict["observation"] = self._create_obs_from_history()
 
         return obs_dict, reward, done, info
+
+
+class PhaseWrapper(gym.Wrapper):
+    """Add phase as input"""
+
+    def __init__(self, env: gym.Env, period: int = 40, n_components: int = 4):
+        obs_space = env.observation_space
+
+        assert len(obs_space.shape) == 1, "Only 1D observation spaces are supported"
+
+        low, high = obs_space.low, obs_space.high
+        low, high = np.concatenate((low, [-1.0] * 2 * n_components)), np.concatenate((high, [1.0] * 2 * n_components))
+
+        env.observation_space = gym.spaces.Box(low=low, high=high, dtype=np.float32)
+
+        super(PhaseWrapper, self).__init__(env)
+        self._current_step = 0
+        self._n_components = n_components
+        self._period = period
+
+    def reset(self) -> GymObs:
+        self._current_step = 0
+        return self._get_obs(self.env.reset())
+
+    def step(self, action: Union[int, np.ndarray]) -> GymStepReturn:
+        self._current_step += 1
+        obs, reward, done, info = self.env.step(action)
+        return self._get_obs(obs), reward, done, info
+
+    def _get_obs(self, obs: np.ndarray) -> np.ndarray:
+        """
+        Concatenate the phase feature to the current observation.
+        """
+        k = 2 * np.pi / self._period
+        phase_feature = []
+        for i in range(1, self._n_components + 1):
+            phase_feature.append(np.cos(i * k * self._current_step))
+            phase_feature.append(np.sin(i * k * self._current_step))
+
+        return np.append(obs, phase_feature)
