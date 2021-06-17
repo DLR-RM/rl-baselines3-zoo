7bf561554d0379ff11bdb627ae86f92197e1aa5c
Justin Terry <justinkterry@gmail.com>
2021-06-17 13:46:27 -0400

    test

--- best_trials.py
+++ best_trials.py
@@ -1,8 +1,25 @@
 import optuna
 study = optuna.create_study(study_name='pistonball21', storage='mysql://root:dummy@10.128.0.28/pistonball21', load_if_exists=True, direction="maximize")
-print(study.best_trial)
+# print(study.best_trial)
+values = []
 for i in study.trials:
-    print(i.value)
+    # print(i.value)
+    values.append(i.value)
+
+ordered_indices = []
+scratch_values = values.copy()
+
+for i in range(len(values)):
+    index = scratch_values.index(max(values))
+    ordered_indices.append(index)
+    scratch_values[index] = -200
+
+for i in ordered_indices:
+    print(values[i])
+
+# pick max element, put its index in new list, set its value to -200, repeat for length of list
+
+
 
 # get max indices
 
