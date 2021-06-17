b4caab19cb1381ca6986ae9d22cfb84387976825
Justin Terry <justinkterry@gmail.com>
2021-06-17 14:26:48 -0400

    attempt json saving

--- best_trials.py
+++ best_trials.py
@@ -1,4 +1,6 @@
 import optuna
+import json
+
 study = optuna.create_study(study_name='pistonball21', storage='mysql://root:dummy@10.128.0.28/pistonball21', load_if_exists=True, direction="maximize")
 # print(study.best_trial)
 values = []
@@ -18,8 +20,15 @@ for i in range(len(values)):
     ordered_indices.append(index)
     scratch_values[index] = -200
 
-for i in ordered_indices:
-    print(values[i])
+for i in range(16):
+    params = study.trials[ordered_indices[i]].params
+    text = json.dumps(params)
+    jsonFile = open(str(i) + ".json", "w+")
+    jsonFile.write(text)
+    jsonFile.close()
+
+# for i in ordered_indices:
+#     print(values[i])
 
 # pick max element, put its index in new list, set its value to -200, repeat for length of list
 
