ca5f112c1bf0d7215bb288aa08224ef40d16b3e2
Justin Terry <justinkterry@gmail.com>
2021-07-27 01:35:18 -0400

    bin bash...?

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 2000000 -optimize --n-trials 150 --sampler tpe --pruner median --study-name $1 --storage mysql://root:dummy@10.128.0.28/$1 &> 0.out &
 sleep 3
 CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 2000000 -optimize --n-trials 150 --sampler tpe --pruner median --study-name $1 --storage mysql://root:dummy@10.128.0.28/$1 &> 1.out &
