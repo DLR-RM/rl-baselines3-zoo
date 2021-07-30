1d35d9e917e70ccdb9160aa8f6156d7420aae958
Justin Terry <justinkterry@gmail.com>
2021-07-30 00:36:26 -0400

    ready for first new test

--- run_test1.sh
+++ run_test1.sh
@@ -1,9 +1,15 @@
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 0 &> eval_0.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 1 &> eval_1.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 2 &> eval_2.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 3 &> eval_3.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 4 &> eval_4.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 5 &> eval_5.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 6 &> eval_6.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 7 &> eval_7.out &
-
--- run_test2.sh
+++ run_test2.sh
@@ -1,8 +1,15 @@
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 8 &> eval_8.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 9 &> eval_9.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 10 &> eval_10.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 11 &> eval_11.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 12 &> eval_12.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 13 &> eval_13.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 14 &> eval_14.out &
+sleep 3
 CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 test_hyperparameters.py 15 &> eval_15.out &
