00e088f51c1b7ab96acdfe71dfcdfc030cc479aa
Antonin Raffin <antonin.raffin@ensta.org>
2021-09-13 09:36:10 +0200

    Add support for loading pickled study

--- parse_hyperparameters.py
+++ parse_hyperparameters.py
@@ -1,10 +1,11 @@
 import argparse
 import json
+import pickle
 
 import optuna
 
 
-def value_key(trial: optuna.trial.Trial):
+def value_key(trial: optuna.trial.Trial) -> float:
     # Returns value of trial object for sorting
     if trial.value is None:
         return float("-inf")
@@ -13,8 +14,9 @@ def value_key(trial: optuna.trial.Trial):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--study-name", help="Study name used during hyperparameter optimization", type=str, default=None)
-parser.add_argument("--storage", help="Database storage path used during hyperparameter optimization", type=str, default=None)
+parser.add_argument("-i", "--study-file", help="Path to a pickle file contained a saved study", type=str)
+parser.add_argument("--study-name", help="Study name used during hyperparameter optimization", type=str)
+parser.add_argument("--storage", help="Database storage path used during hyperparameter optimization", type=str)
 parser.add_argument("--print-n-best-trials", help="Show final return values for n best trials", type=int, default=0)
 parser.add_argument(
     "--save-n-best-hyperparameters",
@@ -24,19 +26,33 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-study = optuna.create_study(study_name=args.study_name, storage=args.storage, load_if_exists=True, direction="maximize")
+if args.study_name is None:
+    assert args.study_file is not None, "No --study-file, nor --study-name were provided."
+    with open(args.study_file, "rb") as f:
+        study = pickle.load(f)
+
+else:
+    assert args.storage is not None, "No storage was specified."
+
+    study = optuna.create_study(
+        study_name=args.study_name,
+        storage=args.storage,
+        load_if_exists=True,
+        direction="maximize",
+    )
+
 values = []
 trials = study.trials
 trials.sort(key=value_key, reverse=True)
 
-for i in trials:
+for trial in trials:
     if len(values) < args.print_n_best_trials:
-        print(i.value)
-    values.append(i.value)
+        print(trial.value)
+    values.append(trial.value)
 
-for i in range(args.save_n_best_hyperparameters):
+
+for i in range(min(args.save_n_best_hyperparameters, len(trials))):
     params = trials[i].params
     text = json.dumps(params)
-    json_file = open(f"hyperparameter_jsons/hyperparameters_{i}.json" + ".json", "w+")
-    json_file.write(text)
-    json_file.close()
+    with open(f"hyperparameter_jsons/hyperparameters_{i}.json", "w+") as json_file:
+        json_file.write(text)
