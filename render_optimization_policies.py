8b4d90cf1bb6f111aac3ae4fca223de8dbbf4f58
J K Terry <justinkterry@gmail.com>
2021-12-04 11:24:47 -0500

    fix

--- render_optimization_policies.py
+++ render_optimization_policies.py
@@ -27,14 +27,14 @@ for policy in policies:
     obs_list = []
     i = 0
     env.reset()
-    reward = 0
+    total_reward = 0
 
     try:
         while True:
             for agent in env.agent_iter():
                 observation, reward, done, _ = env.last()
                 action = (model.predict(observation, deterministic=True)[0] if not done else None)
-                reward += reward
+                total_reward += reward
 
                 env.step(action)
                 i += 1
@@ -45,10 +45,10 @@ for policy in policies:
 
             break
 
-        reward = reward / n_agents
+        total_reward = total_reward / n_agents
         print("writing gif")
         write_gif(
-            obs_list, "./optimization_gifs/" + policy + "_" + str(reward)[:5] + ".gif", fps=15
+            obs_list, "./optimization_gifs/" + policy + "_" + str(total_reward)[:5] + ".gif", fps=15
         )
     except:
         print("error")
