cd749846e6c8c853f8df83056f760fafc788c77d
J K Terry <justinkterry@gmail.com>
2021-12-13 20:02:33 -0500

    1 per gpu

--- run_optimize.sh
+++ run_optimize.sh
@@ -11,12 +11,3 @@ sleep 3
 CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 2000000 --optimization-log-path optimization_policies -optimize --n-trials 150 --sampler tpe --pruner median --study-name $1 --storage mysql://root:dummy@10.128.0.28/$1 &> ./optimize_logs/optimize_2.out &
 sleep 3
 CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 2000000 --optimization-log-path optimization_policies -optimize --n-trials 150 --sampler tpe --pruner median --study-name $1 --storage mysql://root:dummy@10.128.0.28/$1 &> ./optimize_logs/optimize_3.out &
-sleep 3
-CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 2000000 --optimization-log-path optimization_policies -optimize --n-trials 150 --sampler tpe --pruner median --study-name $1 --storage mysql://root:dummy@10.128.0.28/$1 &> ./optimize_logs/optimize_4.out &
-sleep 3
-CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 2000000 --optimization-log-path optimization_policies -optimize --n-trials 150 --sampler tpe --pruner median --study-name $1 --storage mysql://root:dummy@10.128.0.28/$1 &> ./optimize_logs/optimize_5.out &
-sleep 3
-CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 2000000 --optimization-log-path optimization_policies -optimize --n-trials 150 --sampler tpe --pruner median --study-name $1 --storage mysql://root:dummy@10.128.0.28/$1 &> ./optimize_logs/optimize_6.out &
-sleep 3
-CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 train.py --algo ppo --env LunarLanderContinuous-v2 -n 2000000 --optimization-log-path optimization_policies -optimize --n-trials 150 --sampler tpe --pruner median --study-name $1 --storage mysql://root:dummy@10.128.0.28/$1 &> ./optimize_logs/optimize_7.out &
-sleep 3
