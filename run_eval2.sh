c4a4848491f420f723094c215195b59b4ecbc79c
J K Terry <jkterry0@gmail.com>
2022-02-28 12:28:19 -0500

    dream/prophet?

--- run_eval2.sh
+++ run_eval2.sh
@@ -5,8 +5,8 @@ rm -rf mature_policies/*
 
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 8 &> ./eval_logs/eval_8.out &
 sleep 3
-CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 9 &> ./eval_logs/eval_9.out &
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 9 &> ./eval_logs/eval_9.out &
 sleep 3
-CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 10 &> ./eval_logs/eval_10.out &
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 10 &> ./eval_logs/eval_10.out &
 sleep 3
-CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 11 &> ./eval_logs/eval_11.out &
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 eval_hyperparameters.py 11 &> ./eval_logs/eval_11.out &
--- run_render1.sh
+++ run_render1.sh
@@ -5,9 +5,9 @@ rm -rf render_logs/*
 
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 render.py 0 &> ./render_logs/render_0.out &
 CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 render.py 1 &> ./render_logs/render_1.out &
-CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 render.py 2 &> ./render_logs/render_2.out &
-CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 render.py 3 &> ./render_logs/render_3.out &
-CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 render.py 4 &> ./render_logs/render_4.out &
-CUDA_VISIBLE_DEVICES=2 OMP_NUM_THREADS=1 nohup python3 render.py 5 &> ./render_logs/render_5.out &
-CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 render.py 6 &> ./render_logs/render_6.out &
-CUDA_VISIBLE_DEVICES=3 OMP_NUM_THREADS=1 nohup python3 render.py 7 &> ./render_logs/render_7.out &
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 render.py 2 &> ./render_logs/render_2.out &
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 nohup python3 render.py 3 &> ./render_logs/render_3.out &
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 render.py 4 &> ./render_logs/render_4.out &
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 render.py 5 &> ./render_logs/render_5.out &
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 render.py 6 &> ./render_logs/render_6.out &
+CUDA_VISIBLE_DEVICES=1 OMP_NUM_THREADS=1 nohup python3 render.py 7 &> ./render_logs/render_7.out &
