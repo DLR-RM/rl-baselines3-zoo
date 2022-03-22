775162ba86a0d4b66f51e5119ff2d812bdf6019b
Antonin Raffin <antonin.raffin@ensta.org>
2022-03-22 11:23:55 +0100

    Update test

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -5,19 +5,19 @@ def _assert_eq(left, right):
     assert left == right, f"{left} != {right}"
 
 
-# def test_parallel_train():
-args = [
-    "-n",
-    str(1000),
-    "--algo",
-    "ppo",
-    "--env",
-    "CartPole-v1",
-    "-params",
-    "callback:'utils.callbacks.RawStatisticsCallback'",
-    "--tensorboard-log",
-    "/tmp/stable-baselines/",
-]
+def test_raw_stat_callback(tmp_path):
+    args = [
+        "-n",
+        str(200),
+        "--algo",
+        "ppo",
+        "--env",
+        "CartPole-v1",
+        "-params",
+        "callback:'utils.callbacks.RawStatisticsCallback'",
+        "--tensorboard-log",
+        f"{tmp_path}",
+    ]
 
-return_code = subprocess.call(["python", "train.py"] + args)
-_assert_eq(return_code, 0)
+    return_code = subprocess.call(["python", "train.py"] + args)
+    _assert_eq(return_code, 0)
