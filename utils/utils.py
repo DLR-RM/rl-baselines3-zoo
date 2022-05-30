17b38f18eef26b7fa3229485c0d63e0c654b8679
Maximilian Ernestus <maximilian@ernestus.de>
2022-05-30 13:11:40 +0200

    Fix formatting in utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -328,7 +328,7 @@ def get_latest_run_id(log_path: str, env_id: str) -> int:
     max_run_id = 0
     for path in glob.glob(os.path.join(log_path, env_id + "_[0-9]*")):
         run_id = path.split("_")[-1]
-        path_without_run_id = path[:-len(run_id)-1]
+        path_without_run_id = path[: -len(run_id) - 1]
         if path_without_run_id.endswith(env_id) and run_id.isdigit() and int(run_id) > max_run_id:
             max_run_id = int(run_id)
     return max_run_id
