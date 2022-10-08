93b67fcc82d78f9f10828dbb566ec1afc3ce9446
Antonin Raffin <antonin.raffin@ensta.org>
2022-10-08 13:38:45 +0200

    Add support for SBX

---
+++ train_sbx.py
@@ -0,0 +1,15 @@
+import rl_zoo3
+import rl_zoo3.train
+from rl_zoo3.train import train
+from sbx import TQC, DroQ, SAC
+
+rl_zoo3.ALGOS["tqc"] = TQC
+rl_zoo3.ALGOS["droq"] = DroQ
+rl_zoo3.ALGOS["sac"] = SAC
+rl_zoo3.train.ALGOS = rl_zoo3.ALGOS
+rl_zoo3.exp_manager.ALGOS = rl_zoo3.ALGOS
+
+if __name__ == "__main__":
+    train()
+
+
