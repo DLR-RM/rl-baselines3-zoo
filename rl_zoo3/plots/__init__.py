3df922725f310bb73a2174410ed88abebc46d858
Antonin RAFFIN <antonin.raffin@ensta.org>
2023-01-25 18:06:57 +0100

    Upgrade SB3 and add ruff (#345)
    * Upgrade SB3 and add ruff
    
    * Remove `use_auth_token` for push to hub util

--- rl_zoo3/plots/__init__.py
+++ rl_zoo3/plots/__init__.py
@@ -1,3 +1,9 @@
 from rl_zoo3.plots.all_plots import all_plots
 from rl_zoo3.plots.plot_from_file import plot_from_file
 from rl_zoo3.plots.plot_train import plot_train
+
+__all__ = [
+    "all_plots",
+    "plot_from_file",
+    "plot_train",
+]
