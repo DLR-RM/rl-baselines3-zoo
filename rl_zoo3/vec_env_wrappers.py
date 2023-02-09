0a992f1eec123c2d5d45f052ae3989e05c1e2563
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2023-02-09 16:41:00 +0100

    NotImplementedError for vecenv methods

--- rl_zoo3/vec_env_wrappers.py
+++ rl_zoo3/vec_env_wrappers.py
@@ -1,4 +1,3 @@
-import inspect
 from typing import Any, Iterable, List, Optional, Sequence, Type, Union
 
 import gym
@@ -12,31 +11,41 @@ from stable_baselines3.common.vec_env.base_vec_env import VecEnvObs, VecEnvStepR
 VecEnvIndices = Union[None, int, Iterable[int]]
 
 
+def _convert_dtype_to_float32(space: spaces.Space) -> spaces.Space:
+    """
+    Convert the dtype of a space to float32.
+
+    :param space: Space to convert
+    :return: Converted space
+    """
+    if isinstance(space, spaces.Box):
+        space.dtype = np.dtype(np.float32)
+    elif isinstance(space, spaces.Dict):
+        for key, sub_space in space.spaces.items():
+            space.spaces[key] = _convert_dtype_to_float32(sub_space)
+    return space
+
+
 class EnvPoolAdapter(VecEnv):
     """
-    Vectorized environment base class
+    Wrapper for EnvPool to make it compatible with Stable-Baselines3.
 
-    :param venv: the vectorized environment to wrap
-    :param observation_space: the observation space (can be None to load from venv)
-    :param action_space: the action space (can be None to load from venv)
+    :param venv: EnvPool environment
     """
 
     def __init__(self, venv: EnvPool) -> None:
-        # Retrieve the number of environments from the config
         self.venv = venv
         action_space = venv.action_space
         observation_space = venv.observation_space
         # Tmp fix for https://github.com/DLR-RM/stable-baselines3/issues/1145
-        for space in [observation_space, action_space]:
-            if isinstance(space, spaces.Box) and space.dtype == np.float64:
-                space.dtype = np.dtype(np.float32)
+        observation_space = _convert_dtype_to_float32(observation_space)
+        action_space = _convert_dtype_to_float32(action_space)
 
         super().__init__(
-            num_envs=venv.spec.config.num_envs,
+            num_envs=venv.spec.config.num_envs,  # Retrieve the number of environments from the config
             observation_space=observation_space,
             action_space=action_space,
         )
-        self.class_attributes = dict(inspect.getmembers(self.__class__))  # TODO: unused
 
     def reset(self) -> VecEnvObs:
         return self.venv.reset()
@@ -57,26 +66,25 @@ class EnvPoolAdapter(VecEnv):
         return obs, rewards, dones, infos
 
     def close(self) -> None:
-        # No closing method in envpool
-        return None
+        return None  # No closing method in envpool
 
     def get_attr(self, attr_name: str, indices: VecEnvIndices = None) -> List[Any]:
-        raise NotImplementedError()  # TODO: Does envpool support this?
+        raise NotImplementedError("EnvPool does not support get_attr()")
 
     def set_attr(self, attr_name: str, value: Any, indices: VecEnvIndices = None) -> None:
-        raise NotImplementedError()  # TODO: Does envpool support this?
+        raise NotImplementedError("EnvPool does not support set_attr()")
 
     def env_method(self, method_name: str, *method_args, indices: VecEnvIndices = None, **method_kwargs) -> List[Any]:
-        raise NotImplementedError()  # TODO: Does envpool support this?
+        raise NotImplementedError("EnvPool does not support env_method()")
 
     def env_is_wrapped(self, wrapper_class: Type[gym.Wrapper], indices: VecEnvIndices = None) -> List[bool]:
         return [False for _ in range(self.num_envs)]
 
     def get_images(self) -> Sequence[np.ndarray]:
-        raise NotImplementedError()  # TODO: Does envpool support this?
+        raise NotImplementedError("EnvPool does not support get_images()")
 
     def render(self, mode: str = "human") -> Optional[np.ndarray]:
-        raise NotImplementedError()  # TODO: Does envpool support this?
+        raise NotImplementedError("EnvPool does not support render()")
 
     def seed(self, seed: Optional[int] = None) -> Sequence[None]:  # type: ignore[override] # until SB3/#1318 is closed
         # You can only seed EnvPool env by calling envpool.make()
