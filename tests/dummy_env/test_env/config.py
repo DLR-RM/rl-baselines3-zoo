efc1c0b68f61308ab3c570eea45b0f391cf60250
Antonin Raffin <antonin.raffin@ensta.org>
2023-04-12 17:03:12 +0200

    Fix parking dep and test env

---
+++ tests/dummy_env/test_env/config.py
@@ -0,0 +1,6 @@
+hyperparams = {
+    "TestEnv-v0": {
+        "policy": "MlpPolicy",
+        "n_timesteps": 20000,
+    }
+}
--- tests/test_env/test_env/config.py
+++
@@ -1,1 +0,0 @@
-hyperparams = {"TestEnv-v0": {"policy": "MlpPolicy", "n_timesteps": 20000}}
