750bed15d6077eeeae26b0594fd7703fddc1a154
Antonin Raffin <antonin.raffin@ensta.org>
2023-09-03 13:14:29 +0200

    Update docker images

--- scripts/run_docker_cpu.sh
+++ scripts/run_docker_cpu.sh
@@ -6,6 +6,8 @@ cmd_line="$@"
 echo "Executing in the docker (cpu image):"
 echo $cmd_line
 
-docker run -it --rm --network host --ipc=host \
- --mount src=$(pwd),target=/root/code/rl_zoo3,type=bind stablebaselines/rl-baselines3-zoo-cpu:latest\
-  bash -c "cd /root/code/rl_zoo3/ && $cmd_line"
+# Note: --user=root is needed, as the current user id/group id will be used
+# to mount the log directory (and $MAMBAUSER is not root)
+docker run -it --user=root --rm --network host --ipc=host \
+ --mount src=$(pwd),target=/home/mambauser/code/rl_zoo3,type=bind stablebaselines/rl-baselines3-zoo-cpu:latest\
+  bash -c "cd /home/mambauser/code/rl_zoo3/ && $cmd_line"
