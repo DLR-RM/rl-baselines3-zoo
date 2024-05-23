105d5e0f9e52352ed6cd68f105dae57551a64b87
Paul Molodowitch <paul@molodowitch.com>
2024-05-23 11:33:54 -0700

    build_docker.sh - update VERSION to 2.3.0

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -3,7 +3,7 @@
 PARENT=stablebaselines/stable-baselines3
 
 TAG=stablebaselines/rl-baselines3-zoo
-VERSION=2.2.0a1
+VERSION=2.3.0
 
 if [[ ${USE_GPU} == "True" ]]; then
   PARENT="${PARENT}:${VERSION}"
