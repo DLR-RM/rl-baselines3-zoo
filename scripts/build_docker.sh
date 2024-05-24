4649bc38eda2f4272a598a3fc88be7e5e64970f8
Antonin RAFFIN <antonin.raffin@ensta.org>
2024-05-24 13:47:16 +0200

    Read version from txt file

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -3,7 +3,7 @@
 PARENT=stablebaselines/stable-baselines3
 
 TAG=stablebaselines/rl-baselines3-zoo
-VERSION=2.3.0
+VERSION=$(cat ./rl_zoo3/version.txt)
 
 if [[ ${USE_GPU} == "True" ]]; then
   PARENT="${PARENT}:${VERSION}"
