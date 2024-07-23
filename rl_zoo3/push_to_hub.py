078df5929d21f19e0a3a8b782c637ac466d91073
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-07-23 10:01:05 +0200

    Commits from #462

--- rl_zoo3/push_to_hub.py
+++ rl_zoo3/push_to_hub.py
@@ -198,10 +198,9 @@ def package_to_hub(
 
     # Git pull
     repo_local_path = Path(local_repo_path) / repo_name
-    repo = Repository(repo_local_path, clone_from=repo_url)
-    repo.git_pull(rebase=True)
 
-    repo.lfs_track(["*.mp4"])
+    # Retrieve current repo state
+    api.hf_hub_download(repo_id=repo_id, local_dir=local_repo_path)
 
     # Step 1: Save the model
     print("Saving model to:", repo_local_path / model_name)
@@ -269,7 +268,7 @@ def package_to_hub(
     save_model_card(repo_local_path, generated_model_card, metadata)
 
     msg.info(f"Pushing repo {repo_name} to the Hugging Face Hub")
-    repo.push_to_hub(commit_message=commit_message)
+    api.upload_folder(repo_id=repo_id, folder_path=repo_local_path, commit_message=commit_message)
 
     msg.info(f"Your model is pushed to the hub. You can view your model here: {repo_url}")
     return repo_url
